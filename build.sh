f890e847ea92d1babae2846fe44ad4980343b4ff
ntim <niggemann.tim@googlemai.com>
2018-01-06 14:07:18 +0100

    Added version numbers.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-docker build hadoop-base -t ntim/hadoop-base
-docker build hadoop-hdfs-datanode -t ntim/hadoop-hdfs-datanode
-docker build hadoop-hdfs-namenode -t ntim/hadoop-hdfs-namenode
-docker build hadoop-yarn-resourcemanager -t ntim/hadoop-yarn-resourcemanager
-docker build hadoop-yarn-nodemanager -t ntim/hadoop-yarn-nodemanager
-docker build spark -t ntim/spark
-docker build zeppelin -t ntim/zeppelin
+docker build hadoop-base -t ntim/hadoop-base:2.9.0
+docker build hadoop-hdfs-datanode -t ntim/hadoop-hdfs-datanode:2.9.0
+docker build hadoop-hdfs-namenode -t ntim/hadoop-hdfs-namenode:2.9.0
+docker build hadoop-yarn-resourcemanager -t ntim/hadoop-yarn-resourcemanager:2.9.0
+docker build hadoop-yarn-nodemanager -t ntim/hadoop-yarn-nodemanager:2.9.0
+docker build spark -t ntim/spark:2.2.1
+docker build zeppelin -t ntim/zeppelin:0.7.3
