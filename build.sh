abb0096bb2adc3e59e041be087e89bbfffd2fc36
ntim <niggemann.tim@googlemai.com>
2018-01-23 08:49:24 +0100

    Image now based on CentOs 7, added interpreter settings to Zeppelin to use python3.6

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 docker build hadoop-base -t ntim/hadoop-base:latest
 docker build hadoop-hdfs-datanode -t ntim/hadoop-hdfs-datanode:latest
 docker build hadoop-hdfs-namenode -t ntim/hadoop-hdfs-namenode:latest
+docker build hadoop-yarn-base -t ntim/hadoop-yarn-base:latest
 docker build hadoop-yarn-resourcemanager -t ntim/hadoop-yarn-resourcemanager:latest
 docker build hadoop-yarn-nodemanager -t ntim/hadoop-yarn-nodemanager:latest
 docker build spark -t ntim/spark:latest
