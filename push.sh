5fc125084029c9610af62da47adad6bcff786910
ntim <niggemann.tim@googlemai.com>
2018-01-29 15:39:01 +0100

    Added user accounts.

--- push.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-docker push ntim/hadoop-base:latest
-docker push ntim/hadoop-hdfs-datanode:latest
-docker push ntim/hadoop-hdfs-namenode:latest
-docker push ntim/hadoop-yarn-resourcemanager:latest
-docker push ntim/hadoop-yarn-nodemanager:latest
-docker push ntim/spark:latest
-docker push ntim/zeppelin:latest
