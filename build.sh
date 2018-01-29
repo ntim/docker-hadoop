ff0b554eae879fad2f9370f41bae35fde0697ce9
ntim <niggemann.tim@googlemai.com>
2018-01-29 15:36:57 +0100

    Used version tags instead of latest

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-docker build hadoop-base -t ntim/hadoop-base:latest
-docker build hadoop-hdfs-datanode -t ntim/hadoop-hdfs-datanode:latest
-docker build hadoop-hdfs-namenode -t ntim/hadoop-hdfs-namenode:latest
-docker build hadoop-yarn-base -t ntim/hadoop-yarn-base:latest
-docker build hadoop-yarn-resourcemanager -t ntim/hadoop-yarn-resourcemanager:latest
-docker build hadoop-yarn-nodemanager -t ntim/hadoop-yarn-nodemanager:latest
-docker build spark -t ntim/spark:latest
-docker build zeppelin -t ntim/zeppelin:latest
+docker build hadoop-base -t ntim/hadoop-base:2.9.0
+docker build hadoop-hdfs-datanode -t ntim/hadoop-hdfs-datanode:2.9.0
+docker build hadoop-hdfs-namenode -t ntim/hadoop-hdfs-namenode:2.9.0
+docker build hadoop-hdfs-backupnode -t ntim/hadoop-hdfs-backupnode:2.9.0
+docker build hadoop-yarn-base -t ntim/hadoop-yarn-base:2.9.0
+docker build hadoop-yarn-resourcemanager -t ntim/hadoop-yarn-resourcemanager:2.9.0
+docker build hadoop-yarn-nodemanager -t ntim/hadoop-yarn-nodemanager:2.9.0
+docker build spark -t ntim/spark:2.2.1
+docker build zeppelin -t ntim/zeppelin:0.7.3
